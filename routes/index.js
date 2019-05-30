5ac3e77c0f9291a685fc674357171a6bcfcfb8fd
Nabin Bhandari <inabin77@gmail.com>
2019-05-30 22:48:02 +0545

    Added routes for apis

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+import userRoutes from "../modules/user/controller/user.route";
+import cinemaRoutes from "../modules/cinema/controller/cinema.route";
+// import bookingRoutes from '../modules/booking/controller/booking.route'
+import movieRoutes from "../modules/movie/controller/movie.route";
+import seatRoutes from "../modules/seat/controller/seat.route";
+
+import authRoutes from "../modules/auth/auth.route";
+
+import config from "../config";
+
+let BASE_URL = config.app.baseURL;
+
+export default function(app) {
+  app.use(`${BASE_URL}/users`, userRoutes);
+  app.use(`${BASE_URL}/cinemas`, cinemaRoutes);
+  // app.use(`${BASE_URL}/bookings`, bookingRoutes);
+  app.use(`${BASE_URL}/movies`, movieRoutes);
+  app.use(`${BASE_URL}/seats`, seatRoutes);
+  app.use(`${BASE_URL}/auth`, authRoutes);
+}
